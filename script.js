5d282a7c4ee2b121f17c7748379b38f9a23da230
cshdcarlos <carloshernandez4825@gmail.com>
2024-06-26 11:15:06 -0400

    terminar

--- script.js
+++ script.js
@@ -17,6 +17,36 @@ axios.get('https://leonardoapi.onrender.com/songs').then((res) => {
             <img class="imagen__canciones__todas" src="${song.path.front}" alt="">
                 <h4 class="titulo__cantante">${song.title}</h4>
         `
+
+//antes de agregar el elemento
+//agregarle la nteractividad
+
+        div.addEventListener('click', () => {
+//lo que yo escriba aca
+//sucede cuando pase el evento
+
+document.getElementById('portada').setAttribute('src', song.path.front)
+
+document.getElementById('titulo').innerHTML = song.title
+
+
+const audio = document.getElementById('audio')
+audio.setAttribute('src', song.path.audio)
+
+
+document.getElementById('paused').addEventListener('click', () => {
+
+    if (audio.paused){
+        audio.play()
+    }
+    else {audio.pause()}
+})
+
+
+
+
+        })
+
         trackList.appendChild(div)
 
     })
